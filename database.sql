9359affca32d4496ce0ac94208e2181592002f03
François Boiteux <francois.boiteux@epita.fr>
2015-07-11 12:11:57 +0200

    [DB] Add init script

---
+++ database.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE `magrittedb`;
+USE `magrittedb`;
+
+CREATE TABLE `user` (
+  `userid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(127) NOT NULL UNIQUE,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `image` (
+  `imageid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `userid` bigint(20) NOT NULL,
+  `createdate` datetime NOT NULL,
+  PRIMARY KEY (`imageid`),
+  FOREIGN KEY (`userid`) REFERENCES user(`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `user`(username, email, password) VALUES ('flasheur', 'francois.boiteux@gmail.com', 'ncc1701');
+INSERT INTO `image`(name, path, userid, createdate) VALUES ('Test1', 'PathNotExist', 1, NOW());
--- src/main/java/com/mti/Services/ImageService.java
+++ src/main/java/com/mti/Services/ImageService.java
@@ -1,7 +1,25 @@
 package com.mti.Services;
 
+import com.mti.Entities.Image;
+import com.mti.dao.Dao;
+
+import javax.inject.Inject;
+import java.util.Collection;
+
 /**
  * Created by Francois on 11/07/15.
  */
 public class ImageService {
+    @Inject
+    Dao dao;
+
+    public ImageService()
+    {
+    }
+
+    public Collection<Image> GetImages()
+    {
+        Collection<Image> images = dao.findAll(Image.class);
+        return images;
+    }
 }
