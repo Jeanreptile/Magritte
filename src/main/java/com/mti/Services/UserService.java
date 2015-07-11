c83a9b730c5ed81a474cac174f5a112ae70c3e7b
Camille Dollé <dollecamille@gmail.com>
2015-07-11 19:06:29 +0200

    ma

---
+++ src/main/java/com/mti/Services/UserService.java
@@ -0,0 +1,7 @@
+package com.mti.Services;
+
+/**
+ * Created by Camille on 11/07/2015.
+ */
+public class UserService {
+}
---
+++ src/main/java/com/mti/ViewModel/UserViewModel.java
@@ -0,0 +1,7 @@
+package com.mti.ViewModel;
+
+/**
+ * Created by Camille on 11/07/2015.
+ */
+public class UserViewModel {
+}
